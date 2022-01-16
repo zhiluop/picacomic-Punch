14058207e3cfab6b4c0e1c8d163a589964c2132f
zhiluop <54937528+zhiluop@users.noreply.github.com>
2022-01-16 22:30:06 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using picacg;
 namespace picacomic
 {
     class Program
-    {
+    {  
         static async System.Threading.Tasks.Task Main(string[] args)
         {            
             if (args.Length != 2)
